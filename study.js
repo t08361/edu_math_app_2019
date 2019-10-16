c6743515ad050fbd92897906af2569fd62605766
yong jun jo <t08361@naver.com>
2019-10-16 19:01:21 +0900

    #step14

---
+++ study.js
@@ -0,0 +1,12 @@
+function button1_click(){
+    alert("다각형의 내각의 크기의 합은 얼마인가?")
+}
+function button2_click(){
+    alert("다각형의 외각의 크기의 합은 얼마인가?")
+}
+function button3_click(){
+    alert("정다각형의 한 내각의 크기는 얼마인가?")
+}
+function button4_click(){
+    alert("정다각형의 한 외각의 크기는 얼마인가?")
+}

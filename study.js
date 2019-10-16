40d159b9e2ea7242b5c424e7285689ec38a42e52
yong jun jo <t08361@naver.com>
2019-10-16 19:15:38 +0900

    #step14

--- study.js
+++ study.js
@@ -1,12 +1,27 @@
-function button1_click(){
+//study4 text//
+function text4_1() {
     alert("다각형의 내각의 크기의 합은 얼마인가?")
 }
-function button2_click(){
+function text4_2(){
     alert("다각형의 외각의 크기의 합은 얼마인가?")
 }
-function button3_click(){
+function text4_3(){
     alert("정다각형의 한 내각의 크기는 얼마인가?")
 }
-function button4_click(){
+function text4_4(){
+    alert("정다각형의 한 외각의 크기는 얼마인가?")
+}
+
+//study3 text//
+function text3_1() {
+    alert("다각형의 내각의 크기의 합은 얼마인가?")
+}
+function text3_2(){
+    alert("다각형의 외각의 크기의 합은 얼마인가?")
+}
+function text3_3(){
+    alert("정다각형의 한 내각의 크기는 얼마인가?")
+}
+function text3_4(){
     alert("정다각형의 한 외각의 크기는 얼마인가?")
 }

8d2e443538cb4fc586de5d1f4b0fba1741e07618
yong jun jo <t08361@naver.com>
2019-10-16 20:28:14 +0900

    #step14

---
+++ study4.js
@@ -0,0 +1,54 @@
+//study4 text//
+//1-2-3//
+function text1_2_3_1() {
+    alert("동위각, 엇각이란 무엇인가?")
+}
+function text1_2_3_2(){
+    alert("평행선과 동위각,엇각은 어떤 관계가 있는가?")
+}
+function text1_2_3_3(){
+    alert("두 직선이 평행할 조건은 무엇인가?")
+}
+
+//2-1-1//
+function text2_1_1_1() {
+    alert("다각형이란 무엇인가?")
+}
+function text2_1_1_2(){
+    alert("정다각형이란 무엇인가?")
+}
+function text2_1_1_3(){
+    alert("다각형의 대각선의 개수는 어떻게 구하는가?")
+}
+
+//2-1-2//
+function text2_1_2_1() {
+    alert("삼각형의 내각의 크기의 합은 얼마인가?")
+}
+function text2_1_2_2(){
+    alert("삼각형의 내각과 외각 사이에는 어떤 관계가 있는가?")
+}
+
+//2-1-3//
+function text2_1_3_1() {
+    alert("다각형의 내각의 크기의 합은 얼마인가?")
+}
+function text2_1_3_2(){
+    alert("다각형의 외각의 크기의 합은 얼마인가?")
+}
+function text2_1_3_3(){
+    alert("정다각형의 한 내각의 크기는 얼마인가?")
+}
+function text2_1_3_4(){
+    alert("정다각형의 한 외각의 크기는 얼마인가?")
+}
+//2-2-1//
+function text2_2_1_1() {
+    alert("원과 부채꼴이란 무엇인가?")
+}
+function text2_2_1_2(){
+    alert("중심각의 크기와 호의 길이 사이에는 어떤 관계가 있는가?")
+}
+function text2_2_1_3(){
+    alert("중심각의 크기와 현의 길이 사이에는 어떤 관계가 있는가?")
+}

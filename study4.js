f7d06175a9ae8f9c66530e8bda2bdecb783a77bd
yong jun jo <t08361@naver.com>
2019-10-22 16:44:09 +0900

    #step15

--- study4.js
+++ study4.js
@@ -10,6 +10,35 @@ function text1_2_3_3(){
     alert("두 직선이 평행할 조건은 무엇인가?")
 }
 
+//1-3-1//
+function text1_3_1_1() {
+    alert("작도란 무엇인가?")
+}
+function text1_3_1_2(){
+    alert("기본 도형의 작도?")
+}
+
+//1-3-2//
+function text1_3_2_1() {
+    alert("삼각형에서 대변과 대각이란 무엇인가?")
+}
+function text1_3_2_2(){
+    alert("삼각형의 세 변의 길이 사이에는 어떤 관계가 있는가?")
+}
+function text1_3_2_3(){
+    alert("삼각형을 어떻게 작도하는가?")
+}
+//1-3-3//
+function text1_3_3_1() {
+    alert("합동인 도형을 어떻게 나타내는가?")
+}
+function text1_3_3_2(){
+    alert("합동인 도형의 성질은 무엇인가?")
+}
+function text1_3_3_3(){
+    alert("삼각형의 합동 조건이란 무엇인가?")
+}
+
 //2-1-1//
 function text2_1_1_1() {
     alert("다각형이란 무엇인가?")
@@ -51,4 +80,57 @@ function text2_2_1_2(){
 }
 function text2_2_1_3(){
     alert("중심각의 크기와 현의 길이 사이에는 어떤 관계가 있는가?")
+}
+//2-2-2//
+function text2_2_2_1() {
+    alert("원주율(ㅠ))이란 무엇인가?")
+}
+function text2_2_2_2(){
+    alert("원의 둘레의 길이와 넓이는 어떻게 구하는가?")
+}
+function text2_2_2_3(){
+    alert("부채꼴의 호의 길이와 넓이는 어떻게 구하는가?")
+}
+function text2_2_2_4(){
+    alert("부채꼴의 호의 길이와 넓이 사이의 관계?")
+}
+//3-1-1//
+function text3_1_1_1() {
+    alert("다면체란 무엇인가?")
+}
+function text3_1_1_2(){
+    alert("다면체의 종류와 특징은 무엇인가?")
+}
+//3-1-2//
+function text3_1_2_1() {
+    alert("정다면체란 무엇인가?")
+}
+function text3_1_2_2(){
+    alert("정다면체의 종류와 특정은 무엇인가?")
+}
+//3-1-3//
+function text3_1_3_1() {
+    alert("회전체란 무엇인가?")
+}
+function text3_1_3_2(){
+    alert("회전체의 종류와 특징은 무엇인가?")
+}
+function text3_1_3_3() {
+    alert("회전체의 성질은 무엇인가?")
+}
+function text3_1_3_4(){
+    alert("회전체의 전개도를 그릴 수 있는가?")
+}
+//3-2-1//
+function text3_2_1_1() {
+    alert("?")
+}
+function text3_2_1_2(){
+    alert("?")
+}
+function text3_3_1_3() {
+    alert("회전체의 성질은 무엇인가?")
+}
+function text3_3_1_4(){
+    alert("회전체의 전개도를 그릴 수 있는가?")
 }
